0c212a4971cea4cc97c44ddb8d4fa9fc780de9d5
RukshanJS <rjsenanayaka@gmail.com>
2023-03-27 17:22:13 +0530

    feat: added auth page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_client/components/services_list.dart';
+import 'package:flutter_client/views/auth.dart';
 
 import 'components/devices_list.dart';
+import 'views/dashboard.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,21 +12,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(name: 'Flutter Demo Home Page'),
@@ -35,15 +27,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.name});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the name) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String name;
 
   @override
@@ -51,89 +34,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: const TabBar(
-              tabs: [
-                Tab(
-                  icon: Icon(Icons.devices),
-                  text: "Devices",
-                ),
-                Tab(
-                  icon: Icon(Icons.design_services),
-                  text: "Services",
-                ),
-              ],
-            ),
-            title: const Text('Welcome to Kfone!'),
-          ),
-          body: const TabBarView(
-            children: [
-              DevicesList(products: [
-                {
-                  'imageUrl': 'https://picsum.photos/250?image=9',
-                  'name': 'Example device 1',
-                  'description': 'Example description 1',
-                  'manufacturer': 'Example manufacturer 1',
-                  'price': 19.99,
-                },
-                {
-                  'imageUrl': 'https://picsum.photos/250?image=9',
-                  'name': 'Example device 2',
-                  'description': 'Example description 2',
-                  'manufacturer': 'Example manufacturer 2',
-                  'price': 29.99,
-                },
-                {
-                  'imageUrl': 'https://picsum.photos/250?image=9',
-                  'name': 'Example device 3',
-                  'description': 'Example description 3',
-                  'manufacturer': 'Example manufacturer 3',
-                  'price': 39.99,
-                },
-              ]),
-              ServicesList(products: [
-                {
-                  'description': 'decsr',
-                  'name': 'Example service 1',
-                },
-                {
-                  'description': 'decsr',
-                  'name': 'Example service 2',
-                },
-                {
-                  'description': 'decsr',
-                  'name': 'Example service 3',
-                },
-              ]),
-            ],
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: AuthorizationPage(),
     );
   }
 }
